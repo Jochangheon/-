c3d5f1242bb1ee5d6dce2c1695d51c6c1eb667a9
Jochangheon <88309559+Jochangheon@users.noreply.github.com>
2025-09-04 13:12:17 +0900

    Update food crawl.py

--- food crawl.py
+++ food crawl.py
@@ -1,28 +1,24 @@
-import requests
-import json
-
-# Power Automate Flow에서 발급받은 HTTP POST URL 넣으세요
-FLOW_URL = "https://defaultd4398d3154f8451088beffc628e4e5.14.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/bf62fe1628454ec48672433674bdca5c/triggers/manual/paths/invoke/?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=r6gY2Z7mrH27PNiWDh09CrUC2rw04DOC1T9GI-3E-EE"
+import requests, json, os
 
 def main():
+    flow_url = os.environ.get("FLOW_URL")  # GitHub Secrets에서 넘어옴
+    if not flow_url:
+        print("❌ FLOW_URL 환경변수가 없습니다.")
+        return
+
     payload = {
         "title": "🍽️ 오늘의 메뉴",
         "message": "점심 사진 확인하세요",
-        "image_url": "https://ldb-phinf.pstatic.net/20250904_103/1756947656431VPHgD_JPEG/1000024861.jpg"
+        "image_url": "https://ldb-phinf.pstatic.net/test.jpg"
     }
 
     resp = requests.post(
-        FLOW_URL,
+        flow_url,
         headers={"Content-Type": "application/json"},
         data=json.dumps(payload)
     )
-
     print("상태 코드:", resp.status_code)
     print("응답:", resp.text)
-
-if __name__ == "__main__":
-    main()
-
 
 if __name__ == "__main__":
     main()
